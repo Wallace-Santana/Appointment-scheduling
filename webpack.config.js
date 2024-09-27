19f550203a818902c5481e8ee69e0d2b16c04e9e
Wallace-Santana <wallace-santana@outlook.com.br>
2024-09-27 15:26:17 -0300

    terminando config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
-const { trace } = require("console")
-const path = require("path")
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     target: "web",
-    mode:"development",
+    mode: "development",
 
     entry: path.resolve(__dirname, "src", "main.js"),
     output: {
@@ -11,12 +12,46 @@ module.exports = {
         filename: "main.js"
     },
 
-    devServer:{
-        static:{
-            directory: path.join(__dirname, "dist") 
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist")
         },
-        port:  3000,
+        port: 3000,
         open: true,
         liveReload: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, "index.html"),
+            favicon: path.resolve("src", "assets","scissors.svg")
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: path.resolve(__dirname, "src", "assets"), 
+                    to: path.resolve(__dirname, "dist","src","assets") 
+                }
+            ]
+        })
+    ],
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            name: "[name].[ext]",
+                            outputPath: "images/"
+                        }
+                    }
+                ]
+            }
+        ]
     }
-}
+};
